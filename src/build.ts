6bb317faa7e4b65e0a64a10111bc9af1dd159552
Alexander Guryanov <caiiiycuk@gmail.com>
2025-11-04 12:01:16 +0300

    Assing peerId in native builds; introduce jsdos_pentium_mmx

--- src/build.ts
+++ src/build.ts
@@ -5,10 +5,10 @@
 // gulpfile.ts/wasm.ts --> generateBuildInfo
 
 export const Build = {
-    version: "8.3.9 (467a040cbb70abdc922f3f5c64c84c2a)",
-    buildSeed: 1759414227533,
+    version: "8.3.9 (b3f378958675182bb443d19d4c1b8f06)",
+    buildSeed: 1759414566717,
     "wdosbox-x.wasm": {
-        "size": 7912607,
+        "size": 7912632,
         "gzSize": 0
     },
     "wdosbox-x.js": {
@@ -16,7 +16,7 @@ export const Build = {
         "gzSize": 0
     },
     "wdosbox.wasm": {
-        "size": 1458622,
+        "size": 1458645,
         "gzSize": 0
     },
     "wdosbox.js": {
